b4077a2bccb9e990ca394470e6c5b06da00d8863
0sunset0 <chi6465618@naver.com>
2022-03-28 18:11:25 +0900

    commit

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -20,24 +20,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setName("hello");
-
-            Item item = new Item();
-            item.setName("book");
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setItem(item);
-
-            Order order = new Order();
-            order.setMember(member);
-            order.addOrderItem(orderItem);
-
-            em.persist(member);
-            em.persist(item);
-            em.persist(orderItem);
-            em.persist(order);
-
             tx.commit();
         } catch (Exception e){
             tx.rollback();
